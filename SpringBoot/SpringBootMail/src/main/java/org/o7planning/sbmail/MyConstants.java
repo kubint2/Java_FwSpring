e41276c904f0f68a5ed157a25838f7f146513303
binh <binh.lv@tctav.com>
2018-09-18 21:03:50 +0700

    Spring mail

---
+++ SpringBoot/SpringBootMail/src/main/java/org/o7planning/sbmail/MyConstants.java
@@ -0,0 +1,14 @@
+package org.o7planning.sbmail;
+ 
+public class MyConstants {
+ 
+    // Replace with your email here:  
+    public static final String MY_EMAIL = "vanbinhluong92@gmail.com";
+ 
+    // Replace password!!
+    public static final String MY_PASSWORD = "905969312b";
+ 
+    // And receiver!
+    public static final String FRIEND_EMAIL = "vanbinhluong92@gmail.com";
+ 
+}
---
+++ SpringBoot/SpringBootMail/src/main/java/org/o7planning/sbmail/controller/SimpleEmailExampleController.java
@@ -0,0 +1,34 @@
+package org.o7planning.sbmail.controller;
+ 
+import org.o7planning.sbmail.MyConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class SimpleEmailExampleController {
+ 
+    @Autowired
+    public JavaMailSender emailSender;
+ 
+    @ResponseBody
+    @RequestMapping("/sendSimpleEmail")
+    public String sendSimpleEmail() {
+ 
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+         
+        message.setTo(MyConstants.FRIEND_EMAIL);
+        message.setSubject("Test Simple Email");
+        message.setText("Hello, Im testing Simple Email");
+ 
+        // Send Message!
+        this.emailSender.send(message);
+ 
+        return "Email Sent!";
+    }
+ 
+}
